32a35b074694011464922e5208883847425220fc
ZihanSuo <suozh24@mails.tsinghua.edu.cn>
2025-12-11 20:24:09 -0800

    Update and rename 3_readme.py to 3_Methodology.py

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Methodology", layout="wide")
 
-with open("README.md", "r", encoding="utf-8") as f:
+with open("readme.md", "r", encoding="utf-8") as f:
     md_content = f.read()
 
 st.markdown(md_content, unsafe_allow_html=True)
